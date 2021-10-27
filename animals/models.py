5989a7266c3188145d074566c22233c5fb8ce78c
Carlos Martorini <c7.silveira@gmail.com>
2021-10-27 20:59:41 -0300

    Created models animal, group and characteristic

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.FloatField()
+    weight = models.FloatField()
+    sex = models.CharField(max_length=10)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
+    characteristics = models.ManyToManyField('Characteristic', on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    scientific_name = models.CharField(max_length=255)
+
+
+class Characteristic(models.Model):
+    name = models.CharField(max_length=255)
