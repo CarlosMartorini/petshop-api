3f3ff39423441aa72a79d71572189ee0a47658c6
Carlos Martorini <c7.silveira@gmail.com>
2021-10-28 11:49:57 -0300

    Created relationship

--- animals/models.py
+++ animals/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+from characteristics.models import Characteristic
+
 
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     age = models.FloatField()
     weight = models.FloatField()
     sex = models.CharField(max_length=10)
+
+    group = models.ForeignKey('groups.Group', on_delete=models.CASCADE, related_name='animals')
+    characteristics = models.ManyToManyField(Characteristic)
