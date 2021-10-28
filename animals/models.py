7d9805caf3289c959338a55640b1a05e6554bce8
Carlos Martorini <c7.silveira@gmail.com>
2021-10-28 10:36:09 -0300

    Refact groups and characteristics to another apps

--- animals/models.py
+++ animals/models.py
@@ -6,14 +6,3 @@ class Animal(models.Model):
     age = models.FloatField()
     weight = models.FloatField()
     sex = models.CharField(max_length=10)
-    group = models.ForeignKey('Group', on_delete=models.CASCADE)
-    characteristics = models.ManyToManyField('Characteristic', on_delete=models.CASCADE)
-
-
-class Group(models.Model):
-    name = models.CharField(max_length=255)
-    scientific_name = models.CharField(max_length=255)
-
-
-class Characteristic(models.Model):
-    name = models.CharField(max_length=255)
